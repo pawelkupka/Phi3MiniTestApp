3011ee18f150d0f93b08ca3e7d9a778efe0da5f6
pawelkupka <pawel.kupka@gmail.com>
2024-05-10 20:11:55 +0200

    it works

---
+++ Phi3MiniTestApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML.OnnxRuntimeGenAI;
+
+var modelDirectory = args.Length == 2 ? args[1] : @"D:\Source\Phi3MiniTestApp\Phi-3-mini-4k-instruct-onnx\cpu_and_mobile\cpu-int4-rtn-block-32";
+//@"C:\git\oss\Phi-3-mini-4k-instruct-onnx\cuda\cuda-int4-rtn-block-32";
+using var model = new Model(modelDirectory);
+using var tokenizer = new Tokenizer(model);
+
+while (true)
+{
+    Console.Write("Prompt: ");
+    var line = Console.ReadLine();
+    if (line == null) { continue; }
+
+    using var tokens = tokenizer.Encode(line);
+
+    using var generatorParams = new GeneratorParams(model);
+    generatorParams.SetSearchOption("max_length", 2048);
+    generatorParams.SetInputSequences(tokens);
+
+    using var generator = new Generator(model, generatorParams);
+
+    while (!generator.IsDone())
+    {
+        generator.ComputeLogits();
+        generator.GenerateNextToken();
+        var outputTokens = generator.GetSequence(0);
+        var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+        var output = tokenizer.Decode(newToken);
+        Console.Write(output);
+    }
+    Console.WriteLine();
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Phi3MiniTestApp
+
+## Install phi3-mini model
+
+1. git lfs install
+2. git clone https://huggingface.co/microsoft/Phi-3-mini-4k-instruct-onnx
+3. git lfs checkout
